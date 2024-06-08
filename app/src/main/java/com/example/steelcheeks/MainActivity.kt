022a11c8f08924fca2f0bf48084a1df19991198b
Aaron Lucas <aaronlp.ai93@gmail.com>
2024-06-08 16:38:51 +0200

    Fixed nav drawer showing below action bar

--- app/src/main/java/com/example/steelcheeks/MainActivity.kt
+++ app/src/main/java/com/example/steelcheeks/MainActivity.kt
@@ -2,13 +2,12 @@ package com.example.steelcheeks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
-import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 
@@ -25,7 +24,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         drawerLayout = findViewById(R.id.drawer_layout)
 
         //Set up the navigation controller
-        val navHostFragment: NavHostFragment = supportFragmentManager
+        val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
@@ -34,13 +33,12 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             R.id.foodListFragment, R.id.diaryFragment
         ), drawerLayout)
 
-        findViewById<NavigationView>(R.id.nav_view)
-            .setupWithNavController(navController)
-        setupActionBarWithNavController(this, navController, appBarConfiguration)
-    }
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
 
 
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+        findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
+
     }
 }
