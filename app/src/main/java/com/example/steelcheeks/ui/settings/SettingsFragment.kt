bcec2ab307110a208e1775c9e781c0cb1c54a743
Aaron Lucas <aaronlp.ai93@gmail.com>
2024-08-08 19:49:53 +0200

    Added Preferences for settings screen

--- app/src/main/java/com/example/steelcheeks/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/steelcheeks/ui/settings/SettingsFragment.kt
@@ -1,20 +1,11 @@
 package com.example.steelcheeks.ui.settings
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 import com.example.steelcheeks.R
 
-class SettingsFragment : Fragment() {
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
     }
 }
